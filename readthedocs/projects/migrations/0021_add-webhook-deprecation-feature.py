4447a031d92610a569a940c0586a8977f47d4412
RockySpain <Rockyspade@users.noreply.github.com>
2024-08-11 11:54:04 -0700

    Initialize humitos/embed-api-v1

---
+++ readthedocs/projects/migrations/0021_add-webhook-deprecation-feature.py
@@ -0,0 +1,14 @@
+
+"""Add feature for allowing access to deprecated webhook endpoints."""
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0020_add-api-project-proxy'),
+    ]
+
+    operations = [
+    ]
