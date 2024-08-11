a91353551d34c5556fc24350651b3e21bee759dc
RockySpain <Rockyspade@users.noreply.github.com>
2024-08-11 11:50:03 -0700

    Initialize humitos/code-format

---
+++ readthedocs/core/urls/__init__.py
@@ -0,0 +1,18 @@
+"""URL configuration for core app."""
+
+from django.conf.urls import re_path
+
+from readthedocs.constants import pattern_opts
+from readthedocs.core import views
+
+core_urls = [
+    # Random other stuff
+    re_path(
+        (
+            r'^wipe/(?P<project_slug>{project_slug})/'
+            r'(?P<version_slug>{version_slug})/$'.format(**pattern_opts)
+        ),
+        views.wipe_version,
+        name='wipe_version',
+    ),
+]
---
+++ readthedocs/vcs_support/backends/__init__.py
@@ -0,0 +1,9 @@
+"""Listing of all the VCS backends."""
+from . import bzr, git, hg, svn
+
+backend_cls = {
+    'bzr': bzr.Backend,
+    'svn': svn.Backend,
+    'git': git.Backend,
+    'hg': hg.Backend,
+}
