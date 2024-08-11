f6d9755eba790b36217d96b607e1077405dd7c24
RockySpain <Rockyspade@users.noreply.github.com>
2024-08-11 11:56:49 -0700

    Initialize humitos/allow-any-filename-pdf-epub

---
+++ readthedocs/projects/apps.py
@@ -0,0 +1,14 @@
+"""Project app config."""
+
+from django.apps import AppConfig
+
+
+class ProjectsConfig(AppConfig):
+    name = "readthedocs.projects"
+
+    def ready(self):
+        # Load and register notification messages for this application
+        import readthedocs.projects.notifications  # noqa
+        import readthedocs.projects.tasks.builds  # noqa
+        import readthedocs.projects.tasks.search  # noqa
+        import readthedocs.projects.tasks.utils  # noqa
